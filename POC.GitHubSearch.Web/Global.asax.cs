159450f095d570596f7614b096d0d8b66f5e2537
Asaf Shmaya <sh_asaf@hotmail.com>
2020-12-31 13:37:22 +0200

    Added

--- POC.GitHubSearch.Web/Global.asax.cs
+++ POC.GitHubSearch.Web/Global.asax.cs
@@ -1,10 +1,15 @@
-﻿using System;
+﻿using POC.GitHubSearch.Web.App_Start;
+using SimpleInjector;
+using SimpleInjector.Integration.WebApi;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.SessionState;
 
 namespace POC.GitHubSearch.Web
 {
@@ -13,9 +18,40 @@ namespace POC.GitHubSearch.Web
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+            ConfigureApi();
+
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+
+        private static void ConfigureApi()
+        {
+            // 1. Create a new Simple Injector container
+            var container = new Container();
+
+            // 2. Configure the container (register)
+            container.RegisterWebApiControllers(GlobalConfiguration.Configuration);
+
+            // 3. Verify your configuration
+            container.Verify();
+
+            GlobalConfiguration.Configuration.DependencyResolver = new SimpleInjectorWebApiDependencyResolver(container);
+        }
+
+
+        public override void Init()
+        {
+            this.PostAuthenticateRequest += MvcApplication_PostAuthenticateRequest;
+            base.Init();
+        }
+
+        void MvcApplication_PostAuthenticateRequest(object sender, EventArgs e)
+        {
+            System.Web.HttpContext.Current.SetSessionStateBehavior(
+                SessionStateBehavior.Required);
         }
     }
 }
